d7d94780f18e6a7941cddcf924feef848e432f77
GRGSIBERIA <nanashi4129@gmail.com>
2026-06-19 10:44:48 +0900

    get_article_textに依存を注入する

--- yahoo.py
+++ yahoo.py
@@ -20,6 +20,7 @@ RSS_TOPICS_URL = [
     "https://news.yahoo.co.jp/rss/topics/local.xml"
 ]
 
+DB_PATH = "./articles.db"
 
 def get_xml_items(content:str) -> List[Dict[str,str]]:
     root = ET.fromstring(content)
@@ -37,7 +38,7 @@ def get_xml_items(content:str) -> List[Dict[str,str]]:
 
 
 def xml2content(content:str):
-    """メタデータのコンテンツを返す
+    """メタデータのコンテンツをランダムに返す
     return: Hash
         link: link data
         title: title
@@ -80,26 +81,90 @@ def get_second_article(url):
     return body
 
 
-def get_article_text() -> Dict[str, str]:
+def get_select_one_item(conn:sqlite3.Connection):
+    return conn.execute(
+            """
+            SELECT url, title, is_readed
+            FROM articles
+            WHERE is_readed = 0
+            ORDER BY RANDOM()
+            LIMIT 1
+            """
+        ).fetchone()
+
+
+def count_all_articles(conn:sqlite3.Connection) -> int:
+    return conn.execute(
+        """
+        SELECT COUNT(*)
+        FROM articles
+        """
+    ).fetchone()[0]
+
+
+def get_one_offset_article(conn:sqlite3.Connection, offset:int):
+    return conn.execute(
+        """
+        SELECT url, title, is_readed
+        FROM articles
+        ORDER BY url
+        LIMIT 1 OFFSET ?
+        """,
+        (offset),
+    ).fetchone()
+
+
+def get_article_text(conn:sqlite3.Connection=None) -> Dict[str, str]:
     """ニュースフィードからランダムに1件のニュースを取得する
 
-    Returns:
-        Hash: 様々なデータが入っている
-            first_url: 全文読む前のURLが入ってる
-            content: 記事が入っている
-            title: タイトルが入っている
-            pubDate: 出版時期が入っている
-    """
-    url = random.choice(RSS_TOPICS_URL)
-    page = requests.get(url).text
-    logger.info(f"get RSS feed: {url}")
+    Args:
+        conn (sqlite3.Connection): Sqlite3のコネクション、なしでも動く
 
-    meta = xml2content(page)
-    logger.info(f"title: {meta['title']}")
-    logger.info(f"publish date: {meta['pubDate']}")
-    logger.info(f"url: {meta['url']}")
-    meta["first_url"] = get_first_article(meta["url"])
-    meta["content"] = get_second_article(meta["first_url"])
+    Returns:
+        Dict: 様々なデータが入っている
+            - first_url: 全文読む前のURLが入ってる
+            - content: 記事が入っている
+            - title: タイトルが入っている
+            - pubDate: 出版時期が入っている
+    """
+    meta = {}
+    
+    if conn == None:
+        url = random.choice(RSS_TOPICS_URL)
+        page = requests.get(url).text
+        logger.info(f"get RSS feed: {url}")
+
+        meta = xml2content(page)
+        logger.info(f"title: {meta['title']}")
+        logger.info(f"publish date: {meta['pubDate']}")
+        logger.info(f"url: {meta['url']}")
+        meta["first_url"] = get_first_article(meta["url"])
+        meta["content"] = get_second_article(meta["first_url"])
+    
+    if type(conn) == sqlite3.Connection:
+        row = get_select_one_item(conn)
+
+        if row == None:
+            logger.warning("Not readed articles not found")
+            logger.info("Fetching new articles")
+            with conn:
+                get_all_articles(conn)
+        
+        row = get_select_one_item(conn)
+
+        if row == None:
+            logger.warning("Can't fetch new articles")
+            logger.info("Get randon article")
+            unread_count = count_all_articles(conn)
+
+            offset = random.randrange(unread_count)
+            row = get_one_offset_article(conn, offset)
+
+        url, title, _ = row
+
+        meta["first_url"] = get_first_article(url)
+        meta["content"] = get_second_article(meta["first_url"])
+        logger.info(f"{title}")
 
     return meta
 
@@ -141,22 +206,34 @@ def get_all_articles(conn:sqlite3.Connection):
 
 
 def get_articles_first_time():
-    with closing(sqlite3.connect("./articles.db")) as conn:
+    with closing(sqlite3.connect(DB_PATH)) as conn:
         with conn:
             init_table(conn)
             get_all_articles(conn)
+        count = count_all_articles(conn)
+        logger.success(f"Got articles count is {count}")
 
 
 if __name__ == "__main__":
     import time
     DEBUG_SQL = True
     if DEBUG_SQL == True:
-        pass
+        get_articles_first_time()
+
+        with closing(sqlite3.Connection(DB_PATH)) as conn:
+
+            while True:
+                try:
+                    get_article_text(conn)
+                    time.sleep(10.)
+                except Exception as e:
+                    print(e)
+
     else:
         if DEBUG_SQL == False:
             while True:
                 try:
-                    get_article_text()
+                    get_article_text(None)
                     time.sleep(10.)
                 except Exception as e:
                     print(e)
