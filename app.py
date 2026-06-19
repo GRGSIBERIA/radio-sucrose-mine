c4c7034ba6555f61d3d21c18365b7d4aed3f5575
GRGSIBERIA <nanashi4129@gmail.com>
2026-06-19 11:02:45 +0900

    sqlite3を前提にした実装に切り替え

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from openai import OpenAI
 from loguru import logger
 import sys
 from obswebsocket import obsws, requests
+import sqlite3
+from contextlib import closing
 
 client = OpenAI(
     base_url="http://localhost:8000/v1",
@@ -29,28 +31,30 @@ if __name__ == "__main__":
     ws = obsws("localhost", 4455)
     ws.connect()
 
-    while True:
-        try:
-            ws.connect()
+    with closing(sqlite3.Connection("./articles.db")) as conn:
 
-            ws_call(ws, "記事を取得中")
-            article_text = get_article_text()
+        while True:
+            try:
+                ws.connect()
 
-            ws_call(ws, "トークを生成中")
-            talk_text = get_free_talk(client, article_text)
+                ws_call(ws, "記事を取得中")
+                article_text = get_article_text(conn)
 
-            ws_call(ws, "再生中")
-            play_contents(talk_text)
+                ws_call(ws, "トークを生成中")
+                talk_text = get_free_talk(client, article_text)
 
-            ws.disconnect()
+                ws_call(ws, "再生中")
+                play_contents(talk_text)
 
-        except Exception as e:
-            e_type, e_object, e_traceback = sys.exc_info()
-            logger.error(e_type)
-            logger.error(e_object)
-            logger.error(e_traceback)
-            ws.disconnect()
-            continue
+                ws.disconnect()
 
-        finally:
-            ws.disconnect()
+            except Exception as e:
+                e_type, e_object, e_traceback = sys.exc_info()
+                logger.error(e_type)
+                logger.error(e_object)
+                logger.error(e_traceback)
+                ws.disconnect()
+                continue
+
+            finally:
+                ws.disconnect()
